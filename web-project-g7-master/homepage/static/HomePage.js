da54cfa10d90b2e08fd8ef6fac866f1403b7e3f0
shirlehrer <80034393+shirlehrer@users.noreply.github.com>
2021-05-11 17:21:32 +0300

    Add files via upload

---
+++ web-project-g7-master/homepage/static/HomePage.js
@@ -0,0 +1,43 @@
+function openAppointment() {
+  var a = document.getElementById("myAppointment").style.display;
+  if ( a != "block" ){
+    document.getElementById("myAppointment").style.display = "block";
+  }
+}
+
+function closeAppointment() {
+    var b = document.getElementById("myAppointment").style.display;
+  if ( b != "none" ){
+    document.getElementById("myAppointment").style.display = "none";
+  }
+}
+
+function openSignIn() {
+  var a = document.getElementById("mySignIn").style.display;
+  if ( a != "block" ){
+    document.getElementById("mySignIn").style.display = "block";
+  }
+}
+
+function closeSignIn() {
+    var b = document.getElementById("mySignIn").style.display;
+  if ( b != "none" ){
+    document.getElementById("mySignIn").style.display = "none";
+  }
+}
+
+
+
+function openContactUs() {
+  var a = document.getElementById("myContact").style.display;
+  if ( a != "block" ){
+    document.getElementById("myContact").style.display = "block";
+  }
+}
+
+function closeContactUs() {
+    var b = document.getElementById("myContact").style.display;
+  if ( b != "none" ){
+    document.getElementById("myContact").style.display = "none";
+  }
+}
---
+++ web-project-g7-master/shop/static/Shop.js
@@ -0,0 +1,9 @@
+function boldIT( pro) {
+  var a = document.getElementById(pro).style.fontWeight;
+  if ( a == "bold" ){
+    document.getElementById(pro).style.fontWeight = "normal";
+  }
+  if ( a != "bold" ){
+    document.getElementById(pro).style.fontWeight = "bold";
+  }
+}
